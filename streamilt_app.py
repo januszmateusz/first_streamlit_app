1fe4191541af08a3841ea99bfe78391fb16ef896
januszmateusz <januszmateusz@gmail.com>
2023-01-02 19:13:35 +0100

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('Nowa aplikacja do nauki SnowFlake')
+streamlit.header('Jak wygląda header?')
+streamlit.text('Jak wygląda testeks?')
+streamlit.text('Jak wygląda tekst 2?')

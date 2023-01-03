c7b7160f9512378912c92a6911dcf6f5d1e19892
januszmateusz <januszmateusz@gmail.com>
2023-01-03 19:29:42 +0100

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -46,9 +46,9 @@ streamlit.text(my_data_row)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 streamlit.header("Fruit load list contans:")
 
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
